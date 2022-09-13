d0d9f8c8faac2447d338846e32f970dfc2da9233
Lee Zen Been <leezenbeen@yahoo.com.sg>
2022-09-14 01:39:23 +0800

    Add styling to profile page

--- src/api/sampleData.ts
+++ src/api/sampleData.ts
@@ -58,7 +58,7 @@ export const sampleDetailedUser: DetailedUser = {
   },
   matriculationYear: '2022',
   universityCourse: 'Computer Science',
-  bio: `Hi, I'm Bob`,
+  bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
   id: '3',
   name: 'Bob',
   connectionStatus: ConnectionStatus.NOT_CONNECTED,
--- src/pages/user_profile/index.tsx
+++ src/pages/user_profile/index.tsx
@@ -6,11 +6,13 @@ import {
   // IonItem,
   // IonLabel,
   IonPage,
+  IonIcon,
 } from '@ionic/react';
 import AppHeader from '../../components/AppHeader';
 import { useEffect, useState } from 'react';
 import { DetailedUser } from '../../api/types';
 import { getSelfUser } from '../../api/users';
+import { callOutline, mail, paperPlaneOutline } from 'ionicons/icons';
 import styles from './styles.module.scss';
 
 export default function UserProfile() {
@@ -30,51 +32,53 @@ export default function UserProfile() {
         <IonContent fullscreen>
           <IonCard color="primary">
             <IonCardContent>
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <div style={{ display: 'flex', flexDirection: 'row' }}>
-                  <IonAvatar slot="start" className={styles['profile-picture']}>
-                    <img
-                      alt="user picture"
-                      src={
-                        user.profilePic
-                          ? user.profilePic
-                          : 'assets/user_default_icon.svg'
-                      }
-                    ></img>
-                  </IonAvatar>
-                  <div className={styles['user-information']}>
+              <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <IonAvatar slot="start" className={styles['profile-picture']}>
+                  <img
+                    alt="user picture"
+                    src={
+                      user.profilePic
+                        ? user.profilePic
+                        : 'assets/user_default_icon.svg'
+                    }
+                  ></img>
+                </IonAvatar>
+
+                <div className={styles['user-information']}>
+                  <div>
                     <h1 className={styles['name']}>{user.name}</h1>
                     <h2 className={styles['major']}>
                       Majoring in {user.universityCourse}
                     </h2>
+                  </div>
 
-                    <h1 className={styles['bio-header']}>Bio</h1>
-                    <h2 className={styles['bio']}>{user.bio}</h2>
+                  <div className={styles['contact-information']}>
+                    <div>
+                      <IonIcon icon={mail} />
+                      <h2>{user.contact_details.email}</h2>
+                    </div>
+                    <div>
+                      <IonIcon icon={paperPlaneOutline} />
+                      <h2>
+                        {user.contact_details.telegramHandle
+                          ? user.contact_details.telegramHandle
+                          : 'None'}
+                      </h2>
+                    </div>
+                    <div>
+                      <IonIcon icon={callOutline} />
+                      <h2>
+                        {user.contact_details.phoneNumber
+                          ? user.contact_details.phoneNumber
+                          : 'None'}
+                      </h2>
+                    </div>
                   </div>
                 </div>
-
-                <div className={styles['contact-information']}>
-                  <h2>Email: {user.contact_details.email}</h2>
-                  <h2>
-                    Telegram:{' '}
-                    {user.contact_details.telegramHandle
-                      ? user.contact_details.telegramHandle
-                      : 'None'}
-                  </h2>
-                  <h2>
-                    Phone Number:{' '}
-                    {user.contact_details.phoneNumber
-                      ? user.contact_details.phoneNumber
-                      : 'None'}
-                  </h2>
-                </div>
               </div>
+
+              <h1 className={styles['bio-header']}>Bio</h1>
+              <h2 className={styles['bio']}>{user.bio}</h2>
             </IonCardContent>
           </IonCard>
         </IonContent>
