2affbb8e7a9afe130311e54b10788012163d24e0
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2022-09-17 16:11:03 +0800

    Add standardise error handler

--- src/api/authentication.ts
+++ src/api/authentication.ts
@@ -5,8 +5,17 @@
 import axios from 'axios';
 import TokenService from '../util/services/tokenService';
 
-interface ErrorResponse {
-  error_message: string;
+enum RegisterUserErrorReason {
+  EMAIL_ALREADY_USED = 0,
+}
+
+enum VerifyEmailErrorReason {
+  INCORRECT_OTP = 0,
+}
+
+enum LoginErrorReason {
+  UNREGISTERED_EMAIL = 0,
+  INVALID_PASSWORD = 1,
 }
 
 interface VerifyEmailResponseData {
@@ -14,71 +23,18 @@ interface VerifyEmailResponseData {
   refresh_token: string;
 }
 
-class RegisterFieldsError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
-
-class RegisterUserError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
-
-export async function registerUser(
-  nus_email: string,
-  password: string,
-  confirmPassword: string
-) {
-  // These checks should be performed on the main page as well, before this function can ever be called.
-  if (password !== confirmPassword) {
-    throw new RegisterFieldsError('Passwords do not match');
-  }
-  if (!checkValidEmail(nus_email)) {
-    throw new RegisterFieldsError('Invalid NUS Email');
-  }
-
-  try {
-    await axios.post(
-      '/register',
-      { nus_email, password }
-    );
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      const error_message = (error.response.data as ErrorResponse).error_message;
-      //TODO: remove console log
-      console.log('LOGIN ERROR: ', error_message);
-      throw new RegisterUserError(error_message);
-    } else {
-      console.log('UNEXPECTED LOGIN ERROR: ', error);
-      throw error;
-    }
-  }
+export async function registerUser(nus_email: string, password: string) {
+  await axios.post('/register', { nus_email, password });
 }
 
 export async function verifyEmail(nus_email: string, otp: string) {
-  try {
-    const response = await axios.post('/verify', { nus_email, otp });
-    const verifyResponseData = response.data as VerifyEmailResponseData;
+  const response = await axios.post('/verify', { nus_email, otp });
+  const verifyResponseData = response.data as VerifyEmailResponseData;
 
-    TokenService.setTokens({accessToken: verifyResponseData.access_token, refreshToken: verifyResponseData.refresh_token})
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      const error_message = (error.response.data as ErrorResponse)
-        .error_message;
-      //TODO: remove console log
-      console.log('VERIFY EMAIL ERROR: ', error_message);
-      throw new RegisterUserError(error_message);
-    } else {
-      console.log('UNEXPECTED LOGIN ERROR: ', error);
-      throw error;
-    } 
-  }
-}
-
-export async function loginUser(email: string, password: string) {
-
+  TokenService.setTokens({
+    accessToken: verifyResponseData.access_token,
+    refreshToken: verifyResponseData.refresh_token,
+  });
 }
 
 //TODO: email check logic
