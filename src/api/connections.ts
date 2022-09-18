6a33062fe6038696b8c4f39d33e1a466970196ba
Lee Zen Been <leezenbeen@yahoo.com.sg>
2022-09-18 23:17:47 +0800

    Initial connections api

---
+++ src/api/connections.ts
@@ -0,0 +1,39 @@
+/**
+ * API call handlers for modules.
+ */
+
+import axiosInstance from '.';
+import { Connection, ConnectionType, UniModule } from './types';
+
+export async function getConnections(
+  module?: UniModule
+): Promise<Connection[]> {
+  // Optional module to filter by module
+  const response = await axiosInstance.get<Connection[]>('/user/connections', {
+    params: {
+      connection_type: ConnectionType.CONNECTED,
+      module: module,
+    },
+  });
+  return response.data;
+}
+
+// export async function getIncomingConnectionsRequests() {
+
+// }
+
+// export async function getOutgoingConnectionsRequests() {
+
+// }
+
+// export async function acceptIncomingRequest() {
+
+// }
+
+// export async function rejectIncomingRequest() {
+
+// }
+
+// export async function cancelOutgoingRequest() {
+
+// }
